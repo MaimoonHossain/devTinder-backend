ee0011dfde550da8a89f2449ac6ec110557fb439
Maimoon Hossain <ashikasitosh740@gmail.com>
2025-04-10 00:31:12 +0600

    update and delete api integrated

--- src/app.js
+++ src/app.js
@@ -59,6 +59,34 @@ app.get('/feed', async (req, res) => {
   }
 });
 
+app.delete('/user', async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ message: error });
+  }
+});
+
+app.patch('/user', async (req, res) => {
+  const { userId, ...data } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
+      new: true,
+    });
+    res.status(200).json({ message: 'User updated successfully', updatedUser });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ message: error });
+  }
+});
+
 connectDB()
   .then(() => {
     console.log('Connected to MongoDB successfully!');
