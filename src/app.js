00239d3444af45ff8626cfaf28d35ac8fa6e2cbb
Maimoon Hossain <ashikasitosh740@gmail.com>
2025-04-09 23:15:31 +0600

    get api is created

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/database'); // Ensure this is the correct pa
 const User = require('./models/user');
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON requests
+
 app.post('/signup', async (req, res) => {
   const userObj = {
     firstName: 'Maimoon',
@@ -20,6 +22,39 @@ app.post('/signup', async (req, res) => {
     res.status(201).json(savedUser);
   } catch (error) {
     console.error('Error saving user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+//Get user by email
+
+app.get('/user', async (req, res) => {
+  const emailId = req.body.emailId;
+
+  console.log('Email ID:', emailId);
+
+  if (!emailId) {
+    return res.status(400).json({ message: 'Email ID is required' });
+  }
+
+  try {
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
